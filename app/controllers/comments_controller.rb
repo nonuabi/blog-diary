bdda2ea190b013459cd3c610fb6031bee30b8cb4
Abhishke Verma <nonuabi@gmail.com>
2021-11-12 00:33:33 +0530

    Added basic authentication

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+    http_basic_authenticate_with name: "abhishek", password: "kabi kabi", only: :destroy
     def create 
         @article = Article.find(params[:article_id])
         @comment = @article.comments.create(comment_params)
