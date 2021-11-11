f90a2f6a708d001bde9b945df69ff6f8e2ecc232
Abhishke Verma <nonuabi@gmail.com>
2021-11-11 18:08:08 +0530

    added a route for comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'home/index'
 
-  resources :articles
+  resources :articles do 
+    resources :comments
+  end
 
   root 'articles#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
